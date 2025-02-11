e6e8fbf9824350b859c3d1635f106e16c0b0d622
Mehedi Hasan <mehedi.hsn.dev.@gmail.com>
2025-02-11 15:26:22 +0600

    make navbar fixed

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -17,111 +17,130 @@ const Navbar = () => {
         <NavLink to="/all-artifacts">All Artifacts</NavLink>
       </li>
       {user && (
-        <li>
-          <NavLink to="/add-artifacts">Add Artifacts</NavLink>
-        </li>
+        <>
+          <li>
+            <NavLink to="/add-artifacts">Add Artifacts</NavLink>
+          </li>
+          <li>
+            <Link to={'/my-artifacts'}>My Artifacts</Link>
+          </li>
+          <li>
+            <Link to={'/liked-artifacts'}>Liked Artifacts</Link>
+          </li>
+        </>
       )}
     </>
   );
 
   return (
-    <nav className="navbar px-4 bg-primary">
-      {/* nav start */}
-      <div className="navbar-start justify-start">
-        {/* logo for large device */}
-        <NavLink to="/" className="hidden lg:block">
-          <img className="md:h-14 h-10" src={logo} alt="Logo" />
-        </NavLink>
-        {/* user for small device */}
-        <div className="drawer z-10 w-10 h-10">
-          <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
-          <div className="drawer-content flex flex-col items-center justify-center">
-            {/* Page content here */}
-            <label htmlFor="my-drawer-2" className=" drawer-button">
-              <div className="w-10 h-10 overflow-hidden rounded-full">
-                <img
-                  alt="Tailwind CSS Navbar component"
-                  src={user?.photoURL ? user.photoURL : avatar}
-                />
+    <nav className=" bg-primary fixed z-20 w-full">
+      <div className="container mx-auto navbar px-4">
+        {/* nav start */}
+        <div className="navbar-start justify-start">
+          {/* logo for large device */}
+          <NavLink to="/" className="hidden lg:block">
+            <img className="md:h-14 h-10" src={logo} alt="Logo" />
+          </NavLink>
+          {/* user for small device */}
+          <div className="lg:hidden">
+            <div className="drawer z-10 w-10 h-10">
+              <input
+                id="my-drawer-2"
+                type="checkbox"
+                className="drawer-toggle"
+              />
+              <div className="drawer-content flex flex-col items-center justify-center">
+                {/* Page content here */}
+                <label htmlFor="my-drawer-2" className=" drawer-button">
+                  <div className="w-10 h-10 overflow-hidden rounded-full">
+                    <img
+                      alt="Tailwind CSS Navbar component"
+                      src={user?.photoURL ? user.photoURL : avatar}
+                    />
+                  </div>
+                </label>
               </div>
-            </label>
+              <div className="drawer-side">
+                <label
+                  htmlFor="my-drawer-2"
+                  aria-label="close sidebar"
+                  className="drawer-overlay"
+                ></label>
+                <ul className="menu bg-base-200 text-base-content min-h-full w-60 sm:w-80 p-4">
+                  {/* Sidebar content here */}
+                  {navLinks}
+                </ul>
+              </div>
+            </div>
           </div>
-          <div className="drawer-side">
-            <label
-              htmlFor="my-drawer-2"
-              aria-label="close sidebar"
-              className="drawer-overlay"
-            ></label>
-            <ul className="menu bg-base-200 text-base-content min-h-full w-60 sm:w-80 p-4">
-              {/* Sidebar content here */}
+
+          <div className="lg:hidden"></div>
+        </div>
+        {/* nav center */}
+        <div className="navbar-center lg:flex">
+          <div className="hidden md:block">
+            <ul className="menu menu-horizontal px-1 lg:text-lg text-accent">
               {navLinks}
             </ul>
           </div>
-        </div>
-        <div className="lg:hidden"></div>
-      </div>
-      {/* nav center */}
-      <div className="navbar-center lg:flex">
-        <ul className="hidden lg:flex menu menu-horizontal px-1 lg:text-lg text-accent">
-          {navLinks}
-        </ul>
 
-        <NavLink to="/" className="lg:hidden">
-          <img className="h-12" src={logo2} alt="Logo" />
-        </NavLink>
-      </div>
-      {/* nav end */}
-      <div className="navbar-end">
-        <div className="hidden lg:block">
-          {user && (
-            <div className="dropdown dropdown-end">
-              <div
-                tabIndex={0}
-                role="button"
-                className="btn btn-ghost btn-circle avatar"
-                data-tip={user?.displayName || 'User'}
-              >
-                <div className="w-10 rounded-full">
-                  <img
-                    alt="Tailwind CSS Navbar component"
-                    src={user?.photoURL ? user?.photoURL : avatar}
-                  />
+          <NavLink to="/" className="lg:hidden">
+            <img className="h-12" src={logo2} alt="Logo" />
+          </NavLink>
+        </div>
+        {/* nav end */}
+        <div className="navbar-end">
+          <div className="hidden lg:block">
+            {user && (
+              <div className="dropdown dropdown-end">
+                <div
+                  tabIndex={0}
+                  role="button"
+                  className="btn btn-ghost btn-circle avatar"
+                  data-tip={user?.displayName || 'User'}
+                >
+                  <div className="w-10 rounded-full">
+                    <img
+                      alt="Tailwind CSS Navbar component"
+                      src={user?.photoURL ? user?.photoURL : avatar}
+                    />
+                  </div>
                 </div>
+                <ul
+                  tabIndex={0}
+                  className="menu menu-sm dropdown-content bg-tertiary rounded-box mt-3 w-52 p-2 shadow z-10"
+                >
+                  <li>
+                    <Link to={'/my-artifacts'}>My Artifacts</Link>
+                  </li>
+                  <li>
+                    <Link to={'/liked-artifacts'}>Liked Artifacts</Link>
+                    <a></a>
+                  </li>
+                </ul>
               </div>
-              <ul
-                tabIndex={0}
-                className="menu menu-sm dropdown-content bg-tertiary rounded-box mt-3 w-52 p-2 shadow z-10"
+            )}
+          </div>
+
+          {user ? (
+            <Link
+              onClick={() => signOutUser()}
+              to="/"
+              className="btn btn-sm md:btn-md border-none ml-2 bg-secondary text-accent"
+            >
+              Logout
+            </Link>
+          ) : (
+            <>
+              <Link
+                to="/login"
+                className="btn btn-sm md:btn-md border-none bg-secondary text-accent ml-2 hover:text-primary"
               >
-                <li>
-                  <Link to={'/my-artifacts'}>My Artifacts</Link>
-                </li>
-                <li>
-                  <Link to={'/liked-artifacts'}>Liked Artifacts</Link>
-                  <a></a>
-                </li>
-              </ul>
-            </div>
+                Login
+              </Link>
+            </>
           )}
         </div>
-
-        {user ? (
-          <Link
-            onClick={() => signOutUser()}
-            to="/"
-            className="btn btn-sm md:btn-md border-none ml-2 bg-secondary text-accent"
-          >
-            Logout
-          </Link>
-        ) : (
-          <>
-            <Link
-              to="/login"
-              className="btn btn-sm md:btn-md border-none bg-secondary text-accent ml-2 hover:text-primary"
-            >
-              Login
-            </Link>
-          </>
-        )}
       </div>
     </nav>
   );
